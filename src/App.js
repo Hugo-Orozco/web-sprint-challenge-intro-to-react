839bf994cdb5d8295bc3df5de1156dcba8245708
Hugo-Orozco <aceasin@outlook.com>
2021-04-16 16:44:09 -0600

    MVP & Stretch Goals

--- src/App.js
+++ src/App.js
@@ -1,17 +1,114 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+import Character from './components/Character';
+import Axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  let Index = 1;
+
+  const People = new URL('/api/people', 'https://swapi.dev')
+
+  const Endpoint = [
+    `${People.href}/${Index++}/`,
+    `${People.href}/${Index++}/`,
+    `${People.href}/${Index++}/`,
+    `${People.href}/${Index++}/`,
+    `${People.href}/${Index++}/`,
+    `${People.href}/${Index++}/`,
+    `${People.href}/${Index++}/`,
+    `${People.href}/${Index++}/`,
+    `${People.href}/${Index++}/`,
+    `${People.href}/${Index++}/`,
+  ];
+
+  const Promises = [];
+
+  // const [data, setData] = useState(null);
+
+  // useEffect(() => {
+
+  //   Axios.get(Endpoint[0]).then((Res) => {
+
+  //     setData(Res.data);
+
+  //   }).catch((Err) => {
+
+  //     console.error(Err);
+
+  //   });
+
+  // });
+
+  const [data, setData] = useState([]);
+
+  const [open, setOpen] = useState(false);
+
+  const [name, setName] = useState(null);
+
+  const Toggle = (temp) => {
+
+    setName(temp);
+
+    if (temp !== name) {
+      setOpen(true);
+    }
+
+    else {
+      setOpen(!open);
+    }
+
+  };
+
+  const Request = (Endpoint) => new Promise((Resolve, Reject) => {
+
+    Axios.get(Endpoint).then((Res) => {
+
+      Resolve(Res.data);
+
+    }).catch((Err) => {
+
+      Reject(Err);
+
+    });
+
+  });
+
+  Endpoint.forEach((URI) => {
+
+    Promises.push(Request(URI));
+
+  });
+
+  useEffect(() => {
+
+    Promise.all(Promises).then((Res) => {
+
+      setData(Res);
+
+    }).catch((Err) => {
+
+      console.log(Err);
+
+    });
+
+  }, []);
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars - Characters</h1>
+      <div className='Character'>
+        {data.map((data) => {
+          return Character({ name: data.name, birth_year: data.birth_year, gender: data.gender, height: data.height, mass: data.mass, check: name, state: open, toggle: Toggle })
+        })}
+      </div>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,105 @@
 // Write your Character component here
+import React from 'react';
+
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Old versions of Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently supported by Chrome, Edge, Opera and Firefox */
+`;
+
+const Section = styled.div`
+display: flex;
+  background-color: rgba(255, 255, 255, 0.5);
+  margin: 10px 0;
+  padding: 0 20px;
+  outline: 1px solid white;
+  &:hover {
+    box-shadow: 0 0 10px #FFFFFF;
+    text-shadow: 0 0 5px #FFFFFF;
+  }
+`;
+
+const Click = styled.p`
+  color: #FFFFFF;
+  transition: ease-in-out 0.2s;
+  cursor: pointer;
+  outline: none;
+`;
+
+const Text = styled.p`
+  color: #FFFFFF;
+  transition: ease-in-out 0.2s;
+  cursor: default;
+`;
+
+const Hide = styled.div`
+  display: none;
+`;
+
+const Show = styled.div`
+  // position: absolute;
+  display: flex;
+  color: white;
+`;
+
+const Extra = styled.span`
+  margin: auto 0;
+`;
+
+const Character = (props) => {
+
+  const { name, birth_year, gender, height, mass, check, state, toggle } = props;
+
+  const Modal = () => {
+
+    return {
+      Show: () => {
+        return (
+          <Show>
+            <Extra>{'\u200B'} - Gender: {gender.match(/^(n\/a)$/i) ? gender.toUpperCase() : gender.replace(/^(female)$/i, 'Female').replace(/^(male)$/i, 'Male')} | Height: {height.toUpperCase()} | Mass: {mass.toUpperCase()}</Extra>
+          </Show>
+        )
+      },
+      Hide: () => {
+        return (
+          <Hide>Nope</Hide>
+        )
+      }
+    }
+
+  };
+
+  const Handler = () => {
+
+    if (check === name && state) {
+      return Modal().Show()
+    }
+
+    else {
+      return Modal().Hide()
+    }
+
+  }
+
+  return (
+    <Container>
+      <Section>
+        <Click tabIndex={0} role='button' onClick={() => toggle(name)}>{name}</Click>
+        {Handler()}
+      </Section>
+      <Section>
+        <Text>{birth_year.toUpperCase()}</Text>
+      </Section>
+    </Container>
+  );
+
+};
+
+export default Character;
